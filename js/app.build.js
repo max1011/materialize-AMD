6f86cbbd2da60852a12953d09351073d2eff8d7b
Max1011 <max.pcs111@gmail.com>
2015-06-02 15:39:09 +0300

    remodified the code to work with Materialize global

--- js/app.build.js
+++ js/app.build.js
@@ -1,76 +1,86 @@
 // cd js
 // r.js -o app.build.js 
 ({
-	deps: ['jquery'],
-  	paths: {
-    	'jquery': '../bower_components/jquery/dist/jquery',
-    	'hammerjs': 'hammer.min',
-    	'jquery.easing': 'jquery.easing.1.3',
-  	 	'velocity': 'velocity.min',
-  	},
-  	shim: {
-	  	'jquery.easing': {
-	  		deps: ['jquery']
-	  	},
-	  	'animation': {
-	  		deps: ['jquery']
-	  	},
-	  	'jquery.hammer': {
-	  		deps: ['jquery', 'hammerjs', 'waves']
-	  	},
-	  	'collapsible': {
-	  		deps: ['jquery']
-	  	},
-	  	'dropdown': {
-	  		deps: ['jquery']
-	  	},
-	  	'leanModal': {
-	  		deps: ['jquery']
-	  	},
-	  	'materialbox': {
-	  		deps: ['jquery']
-	  	},
-	  	'parallax': {
-	  		deps: ['jquery']
-	  	},
-	  	'tabs': {
-	  		deps: ['jquery']
-	  	},
-	  	'tooltip': {
-	  		deps: ['jquery']
-	  	},
-	  	'sideNav': {
-	  		deps: ['jquery']
-	  	},
-	  	'scrollspy': {
-	  		deps: ['jquery']
-	  	},
-	  	'forms': {
-	  		deps: ['jquery']
-	  	},
-	  	'slider': {
-	  		deps: ['jquery']
-	  	},
-	  	'cards': {
-	  		deps: ['jquery']
-	  	},
-	  	'pushpin': {
-	  		deps: ['jquery']
-	  	},
-	  	'buttons': {
-	  		deps: ['jquery']
-	  	},
-	  	'transitions': {
-	  		deps: ['jquery','scrollFire']
-	  	},
-	  	'scrollFire': {
-	  		deps: ['jquery']
-	  	},
-	  	'waves': {
-	  		exports: 'Waves'
-	  	}
-  	},
-    preserveLicenseComments: false,
-    name: "materialize.amd.js",
-    out: "../dist/js/materialize.amd.js"
+  paths: {
+    'jquery': '../bower_components/jquery/dist/jquery',
+    'hammerjs': 'hammer.min',
+    'jquery.easing': 'jquery.easing.1.3',
+    'velocity': 'velocity.min',
+    'picker': 'date_picker/picker',
+    'picker.date': 'date_picker/picker.date'
+  },
+  shim: {
+    'jquery.easing': {
+      deps: ['jquery']
+    },
+    'animation': {
+      deps: ['jquery']
+    },
+    'jquery.hammer': {
+      deps: ['jquery', 'hammerjs', 'waves']
+    },
+    'global': {
+      deps: ['jquery']
+    },
+    'toasts': {
+      deps: ['global']
+    },
+    'collapsible': {
+      deps: ['jquery']
+    },
+    'dropdown': {
+      deps: ['jquery']
+    },
+    'leanModal': {
+      deps: ['jquery']
+    },
+    'materialbox': {
+      deps: ['jquery']
+    },
+    'parallax': {
+      deps: ['jquery']
+    },
+    'tabs': {
+      deps: ['jquery']
+    },
+    'tooltip': {
+      deps: ['jquery']
+    },
+    'sideNav': {
+      deps: ['jquery']
+    },
+    'scrollspy': {
+      deps: ['jquery']
+    },
+    'forms': {
+      deps: ['jquery', 'global']
+    },
+    'slider': {
+      deps: ['jquery']
+    },
+    'cards': {
+      deps: ['jquery']
+    },
+    'pushpin': {
+      deps: ['jquery']
+    },
+    'buttons': {
+      deps: ['jquery']
+    },
+    'transitions': {
+      deps: ['jquery','scrollFire']
+    },
+    'scrollFire': {
+      deps: ['jquery', 'global']
+    },
+    'waves': {
+      exports: 'Waves'
+    },
+    'character_counter': {
+      deps: ['jquery']
+    }
+  },
+  preserveLicenseComments: false,
+  name: "materialize.amd.js",
+  out: "../dist/js/materialize.amd.js"
 })
--- js/materialize.amd.js
+++ js/materialize.amd.js
@@ -4,98 +4,113 @@ require.config({
     'hammerjs': 'hammer.min',
     'jquery.easing': 'jquery.easing.1.3',
     'velocity': 'velocity.min',
+    'picker': 'date_picker/picker',
+    'picker.date': 'date_picker/picker.date'
   },
   shim: {
-  	'jquery.easing': {
-  		deps: ['jquery']
-  	},
-  	'animation': {
-  		deps: ['jquery']
-  	},
-  	'jquery.hammer': {
-  		deps: ['jquery', 'hammerjs', 'waves']
-  	},
-  	'collapsible': {
-  		deps: ['jquery']
-  	},
-  	'dropdown': {
-  		deps: ['jquery']
-  	},
-  	'leanModal': {
-  		deps: ['jquery']
-  	},
-  	'materialbox': {
-  		deps: ['jquery']
-  	},
-  	'parallax': {
-  		deps: ['jquery']
-  	},
-  	'tabs': {
-  		deps: ['jquery']
-  	},
-  	'tooltip': {
-  		deps: ['jquery']
-  	},
-  	'sideNav': {
-  		deps: ['jquery']
-  	},
-  	'scrollspy': {
-  		deps: ['jquery']
-  	},
-  	'forms': {
-  		deps: ['jquery']
-  	},
-  	'slider': {
-  		deps: ['jquery']
-  	},
-  	'cards': {
-  		deps: ['jquery']
-  	},
-  	'pushpin': {
-  		deps: ['jquery']
-  	},
-  	'buttons': {
-  		deps: ['jquery']
-  	},
-  	'transitions': {
-  		deps: ['jquery','scrollFire']
-  	},
-  	'scrollFire': {
-  		deps: ['jquery']
-  	},
-  	'waves': {
-  		exports: 'Waves'
-  	}
+    'jquery.easing': {
+      deps: ['jquery']
+    },
+    'animation': {
+      deps: ['jquery']
+    },
+    'jquery.hammer': {
+      deps: ['jquery', 'hammerjs', 'waves']
+    },
+    'global': {
+      deps: ['jquery']
+    },
+    'toasts': {
+      deps: ['global']
+    },
+    'collapsible': {
+      deps: ['jquery']
+    },
+    'dropdown': {
+      deps: ['jquery']
+    },
+    'leanModal': {
+      deps: ['jquery']
+    },
+    'materialbox': {
+      deps: ['jquery']
+    },
+    'parallax': {
+      deps: ['jquery']
+    },
+    'tabs': {
+      deps: ['jquery']
+    },
+    'tooltip': {
+      deps: ['jquery']
+    },
+    'sideNav': {
+      deps: ['jquery']
+    },
+    'scrollspy': {
+      deps: ['jquery']
+    },
+    'forms': {
+      deps: ['jquery', 'global']
+    },
+    'slider': {
+      deps: ['jquery']
+    },
+    'cards': {
+      deps: ['jquery']
+    },
+    'pushpin': {
+      deps: ['jquery']
+    },
+    'buttons': {
+      deps: ['jquery']
+    },
+    'transitions': {
+      deps: ['jquery','scrollFire']
+    },
+    'scrollFire': {
+      deps: ['jquery', 'global']
+    },
+    'waves': {
+      exports: 'Waves'
+    },
+    'character_counter': {
+      deps: ['jquery']
+    }
   }
 });
 
 require([
-	'jquery.easing',
-	'animation',
-	'velocity',
-	'hammerjs',
-	'jquery.hammer',
-	'collapsible',
-	'dropdown',
-	'leanModal',
-	'materialbox',
-	'parallax',
-	'tabs',
-	'tooltip',
-	'waves',
-	'toasts',
-	'sideNav',
-	'scrollspy',
-	'forms',
-	'slider',
-	'cards',
-	'pushpin',
-	'buttons',
-	'scrollFire',
-	'transitions',
-	'date_picker/picker',
-	'date_picker/picker.date'
+  'jquery.easing',
+  'animation',
+  'velocity',
+  'hammerjs',
+  'jquery.hammer',
+  'global', // very important do not remove! 
+  'collapsible',
+  'dropdown',
+  'leanModal',
+  'materialbox',
+  'parallax',
+  'tabs',
+  'tooltip',
+  'waves',
+  'toasts',
+  'sideNav',
+  'scrollspy',
+  'forms',
+  'slider',
+  'cards',
+  'pushpin',
+  'buttons',
+  'scrollFire',
+  'transitions',
+  'picker',
+  'picker.date',
+  'character_counter'
 ]);
+
+// all the deps than can be used:
 
 /*
 'jquery.easing',
@@ -103,6 +118,7 @@ require([
 'velocity',
 'hammerjs',
 'jquery.hammer',
+'global'
 'collapsible',
 'dropdown',
 'leanModal',
@@ -121,6 +137,7 @@ require([
 'buttons',
 'scrollFire',
 'transitions',
-'date_picker/picker',
-'date_picker/picker.date'
+'picker',
+'picker.date'
+'character_counter'
 */
